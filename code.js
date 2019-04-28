6c2cc3621a693dd0fbb2f271a3136cb50c3e35fb
MikaCinc <mihajlo.ls00@outlook.com>
2019-04-28 02:12:23 +0200

    Added ROUTING & ANIMATIONS

--- code.js
+++ code.js
@@ -47,7 +47,12 @@ window.onload = function () {
 
 	var elems = document.getElementsByClassName("switch");
 	var contents = document.getElementsByClassName("content_switch");
-	//console.log(contents[3].id.split("_")[1], elems[3].innerHTML.toLowerCase())
+
+	var pageNames = [];
+	for (let n = 0; n < contents.length; n++) {
+		pageNames.push(contents[n].id.split("_")[1]);
+	}
+
 	for (let i = 0; i < elems.length; i++) {
 		elems[i].addEventListener("click", () => {
 			for (let k = 0; k < elems.length; k++) {
@@ -55,18 +60,30 @@ window.onload = function () {
 			}
 			elems[i].className = "switch selected";
 			for (let n = 0; n < contents.length; n++) {
-				if (elems[i].innerHTML.toLowerCase() === contents[n].id.split("_")[1]) {
-					contents[n].style.display = "inline-block";
+				if (elems[i].innerHTML.toLowerCase() === pageNames[n]) {
+					//contents[n].style.display = "inline-block";
+					contents[n].className = 'content_switch active';
+					updateURL(n)
 				} else {
-					contents[n].style.display = "none";
+					//contents[n].style.display = "none";
+					contents[n].className = 'content_switch';
 				}
 			}
 		})
 	}
 
-	// Open default page:
-	var default_page_index = 2;
-	contents[default_page_index].style.display = "inline-block";
+	const updateURL = (n) => {
+		let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + `?page=${pageNames[n]}`;
+		window.history.replaceState({}, '', newurl);
+	}
+
+	// Open default page / Initialisation:
+	var pageInURL = window.location.search.substring(1).split('=')[1];
+	var default_page_index = pageNames.indexOf(pageInURL) >= 0 ? pageNames.indexOf(pageInURL) : 2;
+	//contents[default_page_index].style.display = "inline-block";
+	contents[default_page_index].className = "content_switch active";
+	elems[default_page_index].className = "switch selected";
+	updateURL(default_page_index);
 }
 
 function minc_color() {
